4e9eab38828b31bbafe443cade253312d20ee128
Peter Pham <pttp15122002@gmail.com>
2025-06-16 19:48:19 +1000

    add: navbar

--- src/components/Layout/Header/index.tsx
+++ src/components/Layout/Header/index.tsx
@@ -1,9 +1,11 @@
 import Logo from "../Logo";
+import Navbar from "./Navbar";
 
 export default function Header() {
   return (
     <header>
       <Logo />
+      <Navbar />
     </header>
   );
 }
