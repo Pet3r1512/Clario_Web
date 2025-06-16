2523b4ee1ab0724a0282cb499d8027a7e597e050
Peter Pham <pttp15122002@gmail.com>
2025-06-16 20:58:58 +1000

    chore: remove unused

--- vitest.workspace.ts
+++ vitest.workspace.ts
@@ -1,26 +1,13 @@
 import { defineWorkspace } from "vitest/config";
 import { storybookTest } from "@storybook/addon-vitest/vitest-plugin";
-import path from "node:path";
-import { fileURLToPath } from "node:url";
-
-const dirname =
-  typeof __dirname !== "undefined"
-    ? __dirname
-    : path.dirname(fileURLToPath(import.meta.url));
 
 export default defineWorkspace([
-  // This is the path to your existing Vitest config file
   "./vitest.config.ts",
   {
-    // This is the path to your existing Vite config file
     extends: "./vite.config.ts",
     plugins: [
       storybookTest({
-        // The location of your Storybook config, main.js|ts
-        configDir: path.join(dirname, ".storybook"),
-        // This should match your package.json script to run Storybook
-        // The --ci flag will skip prompts and not open a browser
-        storybookScript: "yarn storybook --ci",
+        storybookScript: "pnpm storybook --no-open",
       }),
     ],
     test: {
