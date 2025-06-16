6a26e4e1bab53c381fc7a01bf56133f419b32152
Peter Pham <pttp15122002@gmail.com>
2025-06-16 18:56:26 +1000

    ops: setup workspace for vitest and storybook

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,10 +1,26 @@
-import { defineConfig } from "vitest/config";
+import { defineConfig, coverageConfigDefaults } from "vitest/config";
 
 export default defineConfig({
   test: {
     globals: true,
     environment: "jsdom",
     setupFiles: ["./setupTests.ts"],
+    coverage: {
+      provider: "v8",
+      reporter: ["text"],
+      exclude: [
+        "storybook-static/*",
+        "src/**/*.stories.tsx",
+        "src/**/*.{config,test}.{ts, js}",
+        "src/**/*.ts",
+        "src/components/ui/*",
+        "src/routeTree.gen.ts",
+        "__vitest_test__/**/*",
+        "*.config.js",
+        "*.config.ts",
+        ...coverageConfigDefaults.exclude,
+      ],
+    },
   },
   resolve: {},
 });
