6b3c6b702bec0bf8914aaf574272ffbd7cb15513
Peter Pham <pttp15122002@gmail.com>
2025-06-17 20:51:41 +1000

    add(test): for navbar component with responsive tests

---
+++ src/stories/Layout/Header/Navbar.stories.tsx
@@ -0,0 +1,41 @@
+import Navbar from "@/components/Layout/Header/Navbar";
+import { Meta, StoryObj } from "@storybook/react-vite";
+import { within } from "@storybook/testing-library";
+import { expect } from "@storybook/jest";
+
+const meta: Meta<typeof Navbar> = {
+  component: Navbar,
+  decorators: [
+    (Story) => (
+      <div className="p-4">
+        <Story />
+      </div>
+    ),
+  ],
+  globals: {
+    viewport: { value: "desktop", isRotated: false },
+  },
+};
+
+export default meta;
+
+type Story = StoryObj<typeof Navbar>;
+
+export const DesktopNavbar: Story = {
+  play: async ({ canvasElement }) => {
+    const canvas = within(canvasElement);
+
+    await expect(canvas.getAllByTestId("navbar")[0]).toBeInTheDocument();
+  },
+};
+
+export const MobileNavbar: Story = {
+  globals: {
+    viewport: { value: "mobile1", isRotated: false },
+  },
+  play: async ({ canvasElement }) => {
+    const canvas = within(canvasElement);
+
+    expect(canvas.getByTestId("navbar")).not.toBeVisible();
+  },
+};
