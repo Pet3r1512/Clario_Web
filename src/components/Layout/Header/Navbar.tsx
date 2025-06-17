709847991bdae2338944b2eb514d6c3e7e0d54e2
Peter Pham <pttp15122002@gmail.com>
2025-06-17 20:23:33 +1000

    add: menu icon and hide navbar on mobiles

--- src/components/Layout/Header/Navbar.tsx
+++ src/components/Layout/Header/Navbar.tsx
@@ -1,28 +1,32 @@
 import ThemeToggle from "@/components/ThemeToggle";
 import navigations from "@/lib/navigations";
 import { cn } from "@/lib/utils";
+import { Menu } from "lucide-react";
 
 export default function Navbar() {
   return (
-    <ul className="flex items-center gap-x-7 text-md lg:text-lg font-semibold">
-      <ThemeToggle />
-      {navigations.map((nav) => {
-        return (
-          <li key={nav.name}>
-            <a
-              className={cn(
-                nav.name === "Sign Up"
-                  ? "bg-primary text-white px-4 py-3 rounded-2xl lg:hover:bg-primary/85"
-                  : "lg:hover:text-primary",
-                "transition-all duration-150 ease-linear",
-              )}
-              href={nav.link}
-            >
-              {nav.name}
-            </a>
-          </li>
-        );
-      })}
-    </ul>
+    <>
+      <ul className="hidden md:flex items-center gap-x-7 text-md lg:text-lg font-semibold">
+        <ThemeToggle />
+        {navigations.map((nav) => {
+          return (
+            <li key={nav.name}>
+              <a
+                className={cn(
+                  nav.name === "Sign Up"
+                    ? "bg-primary text-white px-4 py-3 rounded-2xl lg:hover:bg-primary/85"
+                    : "lg:hover:text-primary",
+                  "transition-all duration-150 ease-linear",
+                )}
+                href={nav.link}
+              >
+                {nav.name}
+              </a>
+            </li>
+          );
+        })}
+      </ul>
+      <Menu className="md:hidden" />
+    </>
   );
 }
