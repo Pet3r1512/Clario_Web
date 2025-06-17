ae9dfc46d9d67b1e30e196266f9c23dbd581182c
Peter Pham <89619124+Pet3r1512@users.noreply.github.com>
2025-06-17 20:25:30 +1000

    #IMP-001: responsive header (#8)
    * imp(style): add different heights for logo on mobiles and tablets
    
    * imp(style): add extra horizontal padding on mobiles
    
    * add: menu icon and hide navbar on mobiles

--- src/components/Layout/Logo.tsx
+++ src/components/Layout/Logo.tsx
@@ -2,7 +2,7 @@ export default function Logo() {
   return (
     <a href="/">
       <img
-        className="w-auto h-32"
+        className="w-auto h-18 md:h-24 lg:h-32"
         src="/logo/Full_Logo.png"
         alt="Clario Logo"
       />
