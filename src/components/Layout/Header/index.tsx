772447da32c6d5431d6fc840b8cbfb240b261261
Peter Pham <pttp15122002@gmail.com>
2025-06-17 20:23:09 +1000

    imp(style): add extra horizontal padding on mobiles

--- src/components/Layout/Header/index.tsx
+++ src/components/Layout/Header/index.tsx
@@ -3,7 +3,7 @@ import Navbar from "./Navbar";
 
 export default function Header() {
   return (
-    <header className="flex justify-between items-center max-w-7xl mx-auto">
+    <header className="flex justify-between items-center max-w-7xl mx-auto px-5 lg:px-0">
       <Logo />
       <Navbar />
     </header>
