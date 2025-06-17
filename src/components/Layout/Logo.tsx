00840822e579527a90db663b23d0a8275c2902e6
Peter Pham <89619124+Pet3r1512@users.noreply.github.com>
2025-06-17 21:02:26 +1000

    #TEST-001: header component (#9)
    * imp(style): add different heights for logo on mobiles and tablets
    
    * imp(style): add extra horizontal padding on mobiles
    
    * add: menu icon and hide navbar on mobiles
    
    * dist: install viewport support for testing
    
    * ops: add new addon viewport to tests
    
    * imp: add testid
    
    * add(test): for Header component
    
    * add(test): for navbar component with responsive tests
    
    * fix: use getByTestID instead
    
    * add: responsive tests for header
    
    * add: testid for menu icon

--- src/components/Layout/Logo.tsx
+++ src/components/Layout/Logo.tsx
@@ -1,6 +1,6 @@
 export default function Logo() {
   return (
-    <a href="/">
+    <a data-testid="logo-wrapper" href="/">
       <img
         className="w-auto h-18 md:h-24 lg:h-32"
         src="/logo/Full_Logo.png"
---
+++ src/stories/Layout/Header/Header.stories.tsx
@@ -0,0 +1,63 @@
+import Header from "@/components/Layout/Header";
+import { Meta, StoryObj } from "@storybook/react-vite";
+import { within } from "@storybook/testing-library";
+import { expect } from "@storybook/jest";
+
+const meta: Meta<typeof Header> = {
+  component: Header,
+  globals: {
+    viewport: {
+      value: "desktop",
+      isRotated: false,
+    },
+  },
+};
+
+export default meta;
+
+type Story = StoryObj<typeof Header>;
+
+export const Default: Story = {
+  render: () => <Header />,
+  play: async ({ canvasElement }) => {
+    const canvas = within(canvasElement);
+
+    const LogoWrapper = canvas.getByTestId("logo-wrapper");
+
+    await expect(LogoWrapper).toBeInTheDocument();
+
+    const Navbar = canvas.getByTestId("navbar");
+
+    await expect(Navbar).toBeInTheDocument();
+  },
+};
+
+export const DesktopHeader: Story = {
+  play: async ({ canvasElement }) => {
+    const canvas = within(canvasElement);
+
+    await expect(canvas.getByTestId("navbar")).toBeVisible();
+
+    await expect(canvas.getByTestId("logo-wrapper")).toBeVisible();
+
+    await expect(canvas.getByTestId("menu-icon")).not.toBeVisible();
+  },
+};
+
+export const MobileHeader: Story = {
+  globals: {
+    viewport: {
+      value: "mobile1",
+      isRotated: false,
+    },
+  },
+  play: async ({ canvasElement }) => {
+    const canvas = within(canvasElement);
+
+    await expect(canvas.getByTestId("logo-wrapper")).toBeVisible();
+
+    await expect(canvas.getByTestId("navbar")).not.toBeVisible();
+
+    await expect(canvas.getByTestId("menu-icon")).toBeVisible();
+  },
+};
