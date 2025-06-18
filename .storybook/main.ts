50af6db0d6a37cde066cdf3d7cfb2ccd45e13594
Peter Pham <pttp15122002@gmail.com>
2025-06-18 21:23:58 +1000

    undo

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,9 +1,4 @@
 import type { StorybookConfig } from "@storybook/react-vite";
-import { fileURLToPath } from "url";
-import { dirname, resolve } from "path";
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
 
 const config: StorybookConfig = {
   stories: ["../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
@@ -12,21 +7,10 @@ const config: StorybookConfig = {
     "@storybook/addon-docs",
     "@storybook/addon-a11y",
     "@storybook/addon-vitest",
-    "@storybook/addon-viewport",
   ],
   framework: {
     name: "@storybook/react-vite",
     options: {},
-  },
-  viteFinal: async (config) => {
-    config.resolve = {
-      ...config.resolve,
-      alias: {
-        ...config.resolve?.alias,
-        "@": resolve(__dirname, "../src"),
-      },
-    };
-    return config;
   },
 };
 export default config;
