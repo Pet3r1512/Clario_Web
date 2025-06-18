9a0ccc413f546a5e4009ab2cbab44e9acbe94f0d
Peter Pham <pttp15122002@gmail.com>
2025-06-18 21:12:19 +1000

    imp: add optional customised classname

--- src/components/Layout/Logo.tsx
+++ src/components/Layout/Logo.tsx
@@ -1,8 +1,10 @@
-export default function Logo() {
+import { cn } from "@/lib/utils";
+
+export default function Logo({ className }: { className?: string }) {
   return (
     <a data-testid="logo-wrapper" href="/">
       <img
-        className="w-auto h-18 md:h-24 lg:h-32"
+        className={cn("w-auto h-18 md:h-24 lg:h-32", className)}
         src="/logo/Full_Logo.png"
         alt="Clario Logo"
       />
