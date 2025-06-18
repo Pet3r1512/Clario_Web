4fc5df644f2966d2a73fbeb2ad57c2a21646bfb2
Peter Pham <pttp15122002@gmail.com>
2025-06-18 21:22:41 +1000

    fix: unknown cn function when testing

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,4 +1,9 @@
 import type { StorybookConfig } from "@storybook/react-vite";
+import { fileURLToPath } from "url";
+import { dirname, resolve } from "path";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 const config: StorybookConfig = {
   stories: ["../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
@@ -12,6 +17,16 @@ const config: StorybookConfig = {
   framework: {
     name: "@storybook/react-vite",
     options: {},
+  },
+  viteFinal: async (config) => {
+    config.resolve = {
+      ...config.resolve,
+      alias: {
+        ...config.resolve?.alias,
+        "@": resolve(__dirname, "../src"),
+      },
+    };
+    return config;
   },
 };
 export default config;
