66b19873fb1da2255eeeea83e90acefe94623509
Peter Pham <pttp15122002@gmail.com>
2025-06-18 20:38:09 +1000

    chore: remove unused svgs

--- src/components/ThemeToggle.tsx
+++ src/components/ThemeToggle.tsx
@@ -6,7 +6,7 @@ const ThemeToggle = () => {
   const { theme, toggleTheme } = useTheme();
 
   return (
-    <label className="relative inline-block">
+    <label className="relative inline-block w-fit">
       <input
         type="checkbox"
         className="peer sr-only"
@@ -25,26 +25,6 @@ const ThemeToggle = () => {
           size={16}
           className="absolute top-[-2.25px] left-[-0.25px] z-100"
         />
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          fill="none"
-          viewBox="0 0 24 24"
-          stroke="currentColor"
-        >
-          <circle
-            cx="12"
-            cy="12"
-            r="5"
-            stroke="currentColor"
-            strokeWidth="2"
-            fill="none"
-          />
-          <path
-            stroke="currentColor"
-            strokeWidth="2"
-            d="M12 1v2m0 18v2m11-11h-2M3 12H1m16.95 7.07l-1.41-1.41M6.34 6.34L4.93 4.93m12.02 0l-1.41 1.41M6.34 17.66l-1.41 1.41"
-          />
-        </svg>
       </span>
       <span
         className={cn(
@@ -57,18 +37,6 @@ const ThemeToggle = () => {
           size={16}
           className="absolute top-[-2.25px] left-[-0.25px] z-100"
         />
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          fill="none"
-          viewBox="0 0 24 24"
-          stroke="currentColor"
-        >
-          <path
-            stroke="currentColor"
-            strokeWidth="2"
-            d="M21 12.79A9 9 0 1111.21 3a7 7 0 109.79 9.79z"
-          />
-        </svg>
       </span>
       <span
         className={cn(
