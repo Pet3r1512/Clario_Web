eb90a2e58f12136371b865a350ffe52d2886582f
Peter Pham <89619124+Pet3r1512@users.noreply.github.com>
2025-06-19 21:42:14 +1000

    #IMP-002: refactor sidebar component (#14)
    * add: auth links for sidebar
    
    * fix: missing fixed size for a tag
    
    * imp: use loop to render auth links
    
    * fix: import

--- src/components/Layout/Logo.tsx
+++ src/components/Layout/Logo.tsx
@@ -2,7 +2,7 @@ import { cn } from "@/lib/utils";
 
 export default function Logo({ className }: { className?: string }) {
   return (
-    <a data-testid="logo-wrapper" href="/">
+    <a data-testid="logo-wrapper" className="w-fit" href="/">
       <img
         className={cn("w-auto h-18 md:h-24 lg:h-32", className)}
         src="/logo/Full_Logo.png"
