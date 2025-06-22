d58b8f9c9256a086bca716d01f6614e35f7b41c3
Peter Pham <pttp15122002@gmail.com>
2025-06-22 19:41:09 +1000

    test: sidebar's behavior

--- src/stories/Layout/Header/Sidebar.stories.tsx
+++ src/stories/Layout/Header/Sidebar.stories.tsx
@@ -1,3 +1,8 @@
+import Sidebar from "@/components/Layout/Header/Sidebar";
+import { Meta, StoryObj } from "@storybook/react-vite";
+import { userEvent, within } from "@storybook/testing-library";
+import { expect } from "@storybook/jest";
+
 const meta: Meta<typeof Sidebar> = {
   component: Sidebar,
   globals: {
@@ -11,3 +16,33 @@ const meta: Meta<typeof Sidebar> = {
 export default meta;
 
 type Story = StoryObj<typeof Sidebar>;
+
+export const DesktopSidebar: Story = {
+  play: async ({ canvasElement }) => {
+    const canvas = within(canvasElement);
+
+    await expect(canvas.getByTestId("menu-icon")).not.toBeVisible();
+  },
+};
+
+export const MobileSidebar: Story = {
+  globals: {
+    viewport: {
+      value: "mobile1",
+      isRotated: false,
+    },
+  },
+  play: async ({ canvasElement }) => {
+    const canvas = within(canvasElement);
+
+    const MenuIcon = canvas.getByRole("button");
+
+    await expect(MenuIcon).toBeInTheDocument();
+
+    await userEvent.click(MenuIcon);
+
+    const bodyCanvas = within(document.body);
+
+    await expect(bodyCanvas.getByRole("dialog")).toBeInTheDocument();
+  },
+};
