e78baf3980830ab9cc5cc440b3e8226b5c29dab6
Peter Pham <89619124+Pet3r1512@users.noreply.github.com>
2025-06-23 22:33:02 +1000

    #FEAT-007: features section (#18)
    * install motion
    
    * add: component from aceternity
    
    * add: 3 feature cards with animations
    
    * feat: highlight features section
    
    * add: new features section to homepage

---
+++ src/components/Layout/Home/Features/FeatureCards.tsx
@@ -0,0 +1,33 @@
+import { HoverEffect } from "@/components/ui/aceternity/CardHoverEffect";
+import { Calendar, ChartNoAxesCombined, Tag } from "lucide-react";
+import { ReactNode } from "react";
+
+export interface FeatureCardData {
+  icon: ReactNode;
+  title: string;
+  desc: string;
+}
+
+const ICON_CLASSNAME = "text-primary";
+
+const features: FeatureCardData[] = [
+  {
+    icon: <Calendar className={ICON_CLASSNAME} />,
+    title: "Smart Icon Tracking",
+    desc: "Set up your salary schedule and recurring income. Clario automatically adds them on the right dates.",
+  },
+  {
+    icon: <Tag className={ICON_CLASSNAME} />,
+    title: "Expense Categorization",
+    desc: "Label your expenses for better tracking. Organize spending into categories like food, entertainment, and more.",
+  },
+  {
+    icon: <ChartNoAxesCombined className={ICON_CLASSNAME} />,
+    title: "Visual Insights",
+    desc: "Beautiful charts and graphs help you understand your spending habits and financial patterns.",
+  },
+];
+
+export default function FeatureCards() {
+  return <HoverEffect className="mt-14" items={features} />;
+}
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,3 +1,4 @@
+import Features from "@/components/Layout/Home/Features";
 import Hero from "@/components/Layout/Home/Hero";
 import Page from "@/components/Layout/Page";
 import { createFileRoute } from "@tanstack/react-router";
@@ -9,8 +10,9 @@ export const Route = createFileRoute("/")({
 function Index() {
   return (
     <Page className="flex items-center justify-center min-h-screen">
-      <section className="flex flex-col gap-y-5 my-24 lg:my-0">
+      <section className="flex flex-col gap-y-24 my-24 lg:my-0">
         <Hero />
+        <Features />
       </section>
     </Page>
   );
