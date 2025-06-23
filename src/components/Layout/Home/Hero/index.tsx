21b544fa0fea451186fa7159fa107b83c5ba3ae4
Peter Pham <89619124+Pet3r1512@users.noreply.github.com>
2025-06-23 21:54:04 +1000

    #TEST-003: hero section (#17)
    * imp: add needed roles
    
    * chore: use section tag instead
    
    * add(test): for hero section

--- src/components/Layout/Home/Hero/index.tsx
+++ src/components/Layout/Home/Hero/index.tsx
@@ -1,19 +1,28 @@
 export default function Hero() {
   return (
-    <div className="lg:my-24 text-center max-w-6xl mx-auto space-y-10">
+    <section
+      aria-label="hero-section"
+      className="lg:my-24 text-center max-w-6xl mx-auto space-y-10"
+    >
       <h1 className="text-5xl font-semibold tracking-tight text-balance text-gray-900 sm:text-7xl">
         Manage Your Finances with{" "}
         <strong className="text-primary underline underline-offset-6">
           Clario
         </strong>
       </h1>
-      <p className="mt-8 text-lg font-medium text-pretty text-gray-500 sm:text-xl/8">
+      <p
+        role="description"
+        className="mt-8 text-lg font-medium text-pretty text-gray-500 sm:text-xl/8"
+      >
         Clario is the <span className="text-primary">all-in-one</span> money
         management tool that helps you track your income, label your expenses,
         and visualize your financial data. Get started today and achieve your
         financial goals.
       </p>
-      <div className="mt-10 flex items-center justify-center gap-x-6">
+      <div
+        role="action-buttons"
+        className="mt-10 flex items-center justify-center gap-x-6"
+      >
         <a
           href="/auth/signin"
           className="rounded-md bg-primary/95 px-3.5 py-2.5 text-sm font-semibold text-white shadow-xs hover:bg-primary focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-primary"
@@ -35,6 +44,6 @@ export default function Hero() {
         </div>
         <div className="absolute -inset-1 rounded-md blur-md bg-gradient-to-br from-primary via-primary/85 to-primary/60 z-10"></div>
       </div>
-    </div>
+    </section>
   );
 }
