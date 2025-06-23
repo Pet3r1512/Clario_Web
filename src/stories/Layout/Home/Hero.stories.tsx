d40ee85a3296468bfc4df457a975a242b8c1c4a8
Peter Pham <pttp15122002@gmail.com>
2025-06-23 21:50:39 +1000

    add(test): for hero section

---
+++ src/stories/Layout/Home/Hero.stories.tsx
@@ -0,0 +1,52 @@
+import Hero from "@/components/Layout/Home/Hero";
+import { expect } from "@storybook/jest";
+import { Meta, StoryObj } from "@storybook/react-vite";
+import { within } from "@storybook/testing-library";
+
+const meta: Meta<typeof Hero> = {
+  component: Hero,
+  globals: {
+    viewport: {
+      value: "desktop",
+      isRotated: false,
+    },
+  },
+};
+
+export default meta;
+
+type Story = StoryObj<typeof Hero>;
+
+export const Default: Story = {
+  play: async ({ canvasElement }) => {
+    const canvas = within(canvasElement);
+
+    const HeroSection = canvas.getByRole("region");
+
+    expect(HeroSection).toBeInTheDocument();
+
+    const HeroTitle = canvas.getByRole("heading");
+
+    expect(HeroTitle).toBeInTheDocument();
+    expect(HeroTitle.textContent).toBe("Manage Your Finances with Clario");
+
+    const HeroDesc = canvas.getByRole("description");
+
+    expect(HeroDesc).toBeInTheDocument();
+    expect(HeroDesc.textContent).toBe(
+      "Clario is the all-in-one money management tool that helps you track your income, label your expenses, and visualize your financial data. Get started today and achieve your financial goals.",
+    );
+
+    const ActionBtns = canvas.getByRole("action-buttons");
+    expect(ActionBtns).toBeInTheDocument();
+
+    const GetStartedBtn = canvas.getByText("Get started");
+    expect(GetStartedBtn).toBeInTheDocument();
+
+    const LearnMoreBtn = canvas.getByText("Learn more");
+    expect(LearnMoreBtn).toBeInTheDocument();
+
+    const DashboardImg = canvas.getByRole("img");
+    expect(DashboardImg).toBeInTheDocument();
+  },
+};
