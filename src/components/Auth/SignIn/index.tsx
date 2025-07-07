2f68859a6050a01d3fee7f6dcdc94939afdb6266
Peter Pham <pttp15122002@gmail.com>
2025-07-07 21:08:46 +1000

    add: main sign in container

---
+++ src/components/Auth/SignIn/index.tsx
@@ -0,0 +1,9 @@
+import SignInForm from "./SignInForm";
+
+export default function SignIn() {
+  return (
+    <article className="w-full flex flex-col justify-center items-center py-8">
+      <SignInForm />
+    </article>
+  );
+}
