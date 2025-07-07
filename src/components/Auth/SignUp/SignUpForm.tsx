c7c3dd7fafce50c839ff53c9f8afa242b47ec0e6
Peter Pham <pttp15122002@gmail.com>
2025-07-07 21:07:53 +1000

    add: coming soon tag for auth via google

--- src/components/Auth/SignUp/SignUpForm.tsx
+++ src/components/Auth/SignUp/SignUpForm.tsx
@@ -38,7 +38,10 @@ export default function SignUpForm({ className }: { className?: string }) {
           </CardDescription>
         </CardHeader>
         <CardContent>
-          <div className="flex flex-col gap-4">
+          <div className="flex flex-col gap-4 relative">
+            <p className="absolute -top-3 -right-4.5 text-white rounded-2xl font-semibold text-xs bg-blue-400 px-1.5 py-0.5">
+              Coming Soon
+            </p>
             <Button variant="outline" className="w-full">
               <GoogleSVG />
               Login with Google
