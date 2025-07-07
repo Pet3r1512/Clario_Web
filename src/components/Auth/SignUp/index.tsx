053876e28a9608a0be2912484308c4aa44efcdd0
Peter Pham <pttp15122002@gmail.com>
2025-07-07 21:07:25 +1000

    style: add space on top and at the bottom for forms

--- src/components/Auth/SignUp/index.tsx
+++ src/components/Auth/SignUp/index.tsx
@@ -2,7 +2,7 @@ import SignUpForm from "./SignUpForm";
 
 export default function SignUp() {
   return (
-    <article className="w-full flex flex-col justify-center items-center">
+    <article className="w-full flex flex-col justify-center items-center py-8">
       <SignUpForm />
     </article>
   );
