ab8e3c7193aa734cf26646a53285173a9f99d9d1
Peter Pham <pttp15122002@gmail.com>
2025-07-27 14:22:57 +1000

    ops: response for signing up via email

--- src/api/users/auth/SignUpEmail.ts
+++ src/api/users/auth/SignUpEmail.ts
@@ -1,13 +1,18 @@
 import { SignUpFormType } from "@/lib/types/signupform";
 
 export default async function SignUpEmail(credentials: SignUpFormType) {
-  const response = await fetch("localhost:8787/trpc/auth.signUpViaEmail", {
-    method: "POST",
-    headers: {
-      "Content-type": "application/json",
+  const response = await fetch(
+    " http://localhost:8787/trpc/auth.signUpViaEmail",
+    {
+      method: "POST",
+      headers: {
+        "Content-type": "application/json",
+      },
+      body: JSON.stringify(credentials),
     },
-    body: JSON.stringify(credentials),
-  });
+  );
+
+  console.log(response);
 
   if (!response.ok) {
     const res = await response.json();
