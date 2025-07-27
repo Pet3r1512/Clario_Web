5440a4e0868ab42849553e694c2bc5a4f7ab7d25
Peter Pham <pttp15122002@gmail.com>
2025-07-27 14:51:19 +1000

    update: server url from env file

--- src/api/users/auth/SignUpEmail.ts
+++ src/api/users/auth/SignUpEmail.ts
@@ -1,16 +1,15 @@
 import { SignUpFormType } from "@/lib/types/signupform";
 
+const SERVER_URL = import.meta.env.VITE_SERVER_URL;
+
 export default async function SignUpEmail(credentials: SignUpFormType) {
-  const response = await fetch(
-    " http://localhost:8787/trpc/auth.signUpViaEmail",
-    {
-      method: "POST",
-      headers: {
-        "Content-type": "application/json",
-      },
-      body: JSON.stringify(credentials),
+  const response = await fetch(`${SERVER_URL}/trpc/auth.signUpViaEmail`, {
+    method: "POST",
+    headers: {
+      "Content-type": "application/json",
     },
-  );
+    body: JSON.stringify(credentials),
+  });
 
   console.log(response);
 
