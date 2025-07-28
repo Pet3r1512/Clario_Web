934b0ce2515ce2f87eb7b753147ca4dc952bb054
Peter Pham <pttp15122002@gmail.com>
2025-07-29 09:30:58 +1000

    imp: apply queryClient to decorator instead

--- src/stories/auth/SignUp/SignUpForm.stories.tsx
+++ src/stories/auth/SignUp/SignUpForm.stories.tsx
@@ -2,20 +2,23 @@ import SignUpForm from "@/components/Auth/SignUp/SignUpForm";
 import { Meta, StoryObj } from "@storybook/react-vite";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
+const queryClient = new QueryClient();
+
 const meta: Meta<typeof SignUpForm> = {
   component: SignUpForm,
+  decorators: [
+    (Story) => (
+      <QueryClientProvider client={queryClient}>
+        <Story />
+      </QueryClientProvider>
+    ),
+  ],
 };
 
 export default meta;
 
 type Story = StoryObj<typeof SignUpForm>;
 
-const queryClient = new QueryClient();
-
-export const Default: Story = {
-  render: () => (
-    <QueryClientProvider client={queryClient}>
-      <SignUpForm />
-    </QueryClientProvider>
-  ),
+export const EmptyForm: Story = {
+  render: () => <SignUpForm />,
 };
