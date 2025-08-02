a307d87b99d54c46469b2b88c62ba7d806529877
Peter Pham <pttp15122002@gmail.com>
2025-08-02 13:03:44 +1000

    test: all fields are valid

--- src/stories/auth/SignUp/SignUpForm.stories.tsx
+++ src/stories/auth/SignUp/SignUpForm.stories.tsx
@@ -1,7 +1,7 @@
 import SignUpForm from "@/components/Auth/SignUp/SignUpForm";
 import { expect } from "@storybook/jest";
 import { Meta, StoryObj } from "@storybook/react-vite";
-import { within } from "@storybook/testing-library";
+import { userEvent, within } from "@storybook/testing-library";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
 const queryClient = new QueryClient();
@@ -60,5 +60,37 @@ export const DefaultForm: Story = {
 
     const signinNav = canvas.getByRole("signin-nav");
     expect(signinNav).toBeInTheDocument();
+  },
+};
+
+export const SuccessForm: Story = {
+  play: async ({ canvasElement }) => {
+    const canvas = within(canvasElement);
+
+    const storybookRoot = document.getElementById("storybook-root");
+
+    const errorMessages = storybookRoot
+      ? within(storybookRoot).queryAllByTestId("form-error-msg")
+      : [];
+
+    const emailInp = canvas.getByRole("email-input");
+    await userEvent.type(emailInp, "exampleemail@gmail.com");
+
+    expect(errorMessages.length).toBe(0);
+
+    const nameInp = canvas.getByRole("name-input");
+    await userEvent.type(nameInp, "Example Name");
+
+    expect(errorMessages.length).toBe(0);
+
+    const passwordInp = canvas.getByRole("password");
+    await userEvent.type(passwordInp, "15122002P");
+
+    expect(errorMessages.length).toBe(0);
+
+    const confirmPasswordInp = canvas.getByRole("confirmPassword");
+    await userEvent.type(confirmPasswordInp, "15122002P");
+
+    expect(errorMessages.length).toBe(0);
   },
 };
