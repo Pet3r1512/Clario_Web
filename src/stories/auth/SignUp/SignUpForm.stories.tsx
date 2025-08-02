fc605077719309c1def30d5cdba4baaf9706bfc7
Peter Pham <pttp15122002@gmail.com>
2025-08-02 12:48:33 +1000

    add: test for default form

--- src/stories/auth/SignUp/SignUpForm.stories.tsx
+++ src/stories/auth/SignUp/SignUpForm.stories.tsx
@@ -1,5 +1,7 @@
 import SignUpForm from "@/components/Auth/SignUp/SignUpForm";
+import { expect } from "@storybook/jest";
 import { Meta, StoryObj } from "@storybook/react-vite";
+import { within } from "@storybook/testing-library";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
 const queryClient = new QueryClient();
@@ -21,4 +23,42 @@ type Story = StoryObj<typeof SignUpForm>;
 
 export const EmptyForm: Story = {
   render: () => <SignUpForm />,
+};
+
+export const DefaultForm: Story = {
+  play: async ({ canvasElement }) => {
+    const canvas = within(canvasElement);
+
+    const signupFormContainer = canvas.getByTestId("signup-form-container");
+    expect(signupFormContainer).toBeInTheDocument();
+
+    const clarioText = canvas.getAllByText("Clario");
+    expect(clarioText[0]).toBeInTheDocument();
+
+    const loginWithGoogleBtn = canvas.getByText("Login with Google");
+    expect(loginWithGoogleBtn).toBeInTheDocument();
+
+    expect(canvas.getByText("Or continue with")).toBeInTheDocument();
+
+    expect(canvas.getByRole("form")).toBeInTheDocument();
+
+    expect(canvas.getByText("Email")).toBeInTheDocument();
+
+    const textboxes = canvas.getAllByRole("textbox");
+    expect(textboxes.length).toBe(2);
+
+    const passwordInp = canvas.getByRole("password");
+    expect(passwordInp).toBeInTheDocument();
+
+    const confirmPasswordInp = canvas.getByRole("confirmPassword");
+    expect(confirmPasswordInp).toBeInTheDocument();
+
+    const submitBtn = canvas.getByRole("submit-btn");
+    expect(submitBtn).toBeInTheDocument();
+
+    expect(submitBtn.textContent).toBe("Create New Account");
+
+    const signinNav = canvas.getByRole("signin-nav");
+    expect(signinNav).toBeInTheDocument();
+  },
 };
