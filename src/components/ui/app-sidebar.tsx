0cbfa4e584c5a805e51e9e0010816a13e82827c1
Peter Pham <pttp15122002@gmail.com>
2025-08-16 18:27:52 +1000

    add: sidebar sections

--- src/components/ui/app-sidebar.tsx
+++ src/components/ui/app-sidebar.tsx
@@ -1,19 +1,55 @@
 import {
   Sidebar,
-  SidebarContent,
   SidebarFooter,
-  SidebarGroup,
+  SidebarGroupContent,
   SidebarHeader,
+  SidebarMenu,
+  SidebarMenuButton,
+  SidebarMenuItem,
 } from "@/components/ui/sidebar";
+import { LayoutDashboard, Banknote, Landmark, Coins } from "lucide-react";
+
+const items = [
+  {
+    title: "Dashboard",
+    url: "dashboard/",
+    icon: <LayoutDashboard />,
+  },
+  {
+    title: "Transactions",
+    url: "dashboard/transactions/",
+    icon: <Banknote />,
+  },
+  {
+    title: "Income",
+    url: "dashboard/income/",
+    icon: <Landmark />,
+  },
+  {
+    title: "Expenses",
+    url: "dashborad/expenses/",
+    icon: <Coins />,
+  },
+];
 
 export function AppSidebar() {
   return (
     <Sidebar>
       <SidebarHeader />
-      <SidebarContent>
-        <SidebarGroup />
-        <SidebarGroup />
-      </SidebarContent>
+      <SidebarGroupContent>
+        <SidebarMenu>
+          {items.map((item) => (
+            <SidebarMenuItem key={item.title}>
+              <SidebarMenuButton asChild>
+                <a href={item.url}>
+                  {item.icon}
+                  <span>{item.title}</span>
+                </a>
+              </SidebarMenuButton>
+            </SidebarMenuItem>
+          ))}
+        </SidebarMenu>
+      </SidebarGroupContent>
       <SidebarFooter />
     </Sidebar>
   );
