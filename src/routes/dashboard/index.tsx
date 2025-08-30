4bae8ba516618370cd5992431b8a2546dd7782ab
Peter Pham <pttp15122002@gmail.com>
2025-08-30 13:13:20 +1000

    chore: remove unused

--- src/routes/dashboard/index.tsx
+++ src/routes/dashboard/index.tsx
@@ -1,18 +1,10 @@
 import DashboardLayout from "@/components/Dashboard/DashboardLayout";
-import { useCurrentUrl } from "@/hooks/useCurrentUrl";
 import { createFileRoute } from "@tanstack/react-router";
-import { useEffect } from "react";
 
 export const Route = createFileRoute("/dashboard/")({
   component: RouteComponent,
 });
 
 function RouteComponent() {
-  const currUrl = useCurrentUrl();
-
-  useEffect(() => {
-    console.log(currUrl);
-  }, [currUrl]);
-
   return <DashboardLayout>Dashboard</DashboardLayout>;
 }
