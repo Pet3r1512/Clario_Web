f10d37db213ffb74f0e532f71f320e0e5dd34bf8
Peter Pham <pttp15122002@gmail.com>
2025-08-30 12:38:12 +1000

    imp: move dashboard index file to better location

--- src/routes/dashboard/index.tsx
+++ src/routes/dashboard/index.tsx
@@ -1,12 +1,20 @@
 import { AppSidebar } from "@/components/ui/app-sidebar";
 import { SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar";
+import { useCurrentUrl } from "@/hooks/useCurrentUrl";
 import { createFileRoute } from "@tanstack/react-router";
+import { useEffect } from "react";
 
-export const Route = createFileRoute("/dashboard")({
+export const Route = createFileRoute("/dashboard/")({
   component: RouteComponent,
 });
 
 function RouteComponent() {
+  const currUrl = useCurrentUrl();
+
+  useEffect(() => {
+    console.log(currUrl);
+  }, [currUrl]);
+
   return (
     <SidebarProvider className="p-5">
       <AppSidebar />
