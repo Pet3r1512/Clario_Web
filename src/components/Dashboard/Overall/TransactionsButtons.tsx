39ccb44deeec069092595d93ec611e670019fdd5
Peter Pham <pttp15122002@gmail.com>
2025-09-03 21:48:30 +1000

    add: component for adding income and expense buttons

---
+++ src/components/Dashboard/Overall/TransactionsButtons.tsx
@@ -0,0 +1,14 @@
+import { Button } from "@/components/ui/button";
+
+export default function TransactionButtons() {
+  return (
+    <div className="flex items-center gap-x-5 justify-end">
+      <Button className="bg-primary hover:bg-primary/90 text-white rounded-2xl text-lg h-10.5">
+        + Add Income
+      </Button>
+      <Button className="bg-white text-black border-[0.5px] border-black hover:bg-white/90 rounded-2xl text-lg h-10.5">
+        + Add Expense
+      </Button>
+    </div>
+  );
+}
