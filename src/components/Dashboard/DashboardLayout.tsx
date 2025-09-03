50a471db69cefae7194b2775597a4292888e9159
Peter Pham <pttp15122002@gmail.com>
2025-09-03 21:10:34 +1000

    imp: use same style of dashboard's page name

--- src/components/Dashboard/DashboardLayout.tsx
+++ src/components/Dashboard/DashboardLayout.tsx
@@ -2,12 +2,21 @@ import { ReactNode } from "react";
 import { SidebarProvider, SidebarTrigger } from "../ui/sidebar";
 import { AppSidebar } from "../ui/app-sidebar";
 
-export default function DashboardLayout({ children }: { children: ReactNode }) {
+export default function DashboardLayout({
+  children,
+  section,
+}: {
+  children: ReactNode;
+  section?: string;
+}) {
   return (
     <SidebarProvider className="p-5">
       <AppSidebar />
       <main>
-        <SidebarTrigger />
+        <div>
+          <SidebarTrigger />
+          <p className="lg:text-3xl font-bold">{section}</p>
+        </div>
         {children}
       </main>
     </SidebarProvider>
