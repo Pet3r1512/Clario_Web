aa3c8137df678bc2b2732bb8c90e5c2f4ef6185e
Peter Pham <pttp15122002@gmail.com>
2025-09-03 21:35:05 +1000

    add: component for displaying data in overall section

---
+++ src/components/Dashboard/Overall/Data.tsx
@@ -0,0 +1,23 @@
+import { Card } from "@/components/ui/card";
+import { ReactNode } from "react";
+
+export default function Data({
+  data,
+}: {
+  data: {
+    name: string;
+    subtitle: string;
+    icon: ReactNode;
+  };
+}) {
+  return (
+    <Card className="w-full py-3.5 px-5 gap-2.5 shadow-lg lg:hover:shadow-2xl transition-all duration-150 ease-linear">
+      <div className="flex items-center justify-between">
+        <p className="lg:text-xl font-extrabold">{data.name}</p>
+        {data.icon}
+      </div>
+      <p className="lg:text-xl font-semibold">$2000.00</p>
+      <p className="text-gray-400 lg:text-sm">{data.subtitle}</p>
+    </Card>
+  );
+}
