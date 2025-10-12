da98e71246f6a72284040478483eb649b122b9de
Peter Pham <pttp15122002@gmail.com>
2025-10-12 20:03:57 +1100

    imp: protect dashboard page

--- src/routes/dashboard/index.tsx
+++ src/routes/dashboard/index.tsx
@@ -2,12 +2,28 @@ import DashboardLayout from "@/components/Dashboard/DashboardLayout";
 import Overall from "@/components/Dashboard/Overall";
 import TransactionButtons from "@/components/Dashboard/Overall/TransactionsButtons";
 import { createFileRoute } from "@tanstack/react-router";
+import { useEffect } from "react";
+import { useNavigate } from "@tanstack/react-router";
+import useAuth from "@/hooks/useAuth";
 
 export const Route = createFileRoute("/dashboard/")({
   component: RouteComponent,
 });
 
 function RouteComponent() {
+  const { isAuthenticated } = useAuth();
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (!isAuthenticated) {
+      navigate({ to: "/auth/signin" });
+    }
+  }, [isAuthenticated, navigate]);
+
+  if (!isAuthenticated) {
+    return null;
+  }
+
   return (
     <DashboardLayout section="Dashboard">
       <TransactionButtons />
