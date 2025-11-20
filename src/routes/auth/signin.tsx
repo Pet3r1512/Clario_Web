163c687e282aebf4b1e8ca72eec327d144c055d1
Peter Pham <89619124+Pet3r1512@users.noreply.github.com>
2025-11-20 16:52:57 +1100

    #FEAT-016: navigate to dashboard for signed in user (#47)

--- src/routes/auth/signin.tsx
+++ src/routes/auth/signin.tsx
@@ -1,12 +1,25 @@
 import SignIn from "@/components/Auth/SignIn";
 import Page from "@/components/Layout/Page";
-import { createFileRoute } from "@tanstack/react-router";
+import useAuth from "@/hooks/useAuth";
+import { createFileRoute, useNavigate } from "@tanstack/react-router";
+import { useEffect } from "react";
 
 export const Route = createFileRoute("/auth/signin")({
   component: RouteComponent,
 });
 
 function RouteComponent() {
+  const { isAuthenticated, isLoading } = useAuth();
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (!isLoading && isAuthenticated) {
+      navigate({
+        to: "/dashboard",
+      });
+    }
+  }, [isAuthenticated, isLoading, navigate]);
+
   return (
     <Page>
       <SignIn />
