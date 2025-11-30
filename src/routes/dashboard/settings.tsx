d9225f3b716b7e8696ee0ee92ab8c4679e3eda5f
Peter Pham <pttp15122002@gmail.com>
2025-11-30 18:06:24 +1100

    chore: remove unused

--- src/routes/dashboard/settings.tsx
+++ src/routes/dashboard/settings.tsx
@@ -1,26 +1,11 @@
 import DashboardLayout from "@/components/Dashboard/DashboardLayout";
-import useAuth from "@/hooks/useAuth";
-import { createFileRoute, useNavigate } from "@tanstack/react-router";
-import { useEffect } from "react";
+import { createFileRoute } from "@tanstack/react-router";
 
 export const Route = createFileRoute("/dashboard/settings")({
   component: RouteComponent,
 });
 
 function RouteComponent() {
-  const { isAuthenticated, isLoading } = useAuth();
-  const navigate = useNavigate();
-
-  useEffect(() => {
-    if (!isLoading && !isAuthenticated) {
-      navigate({ to: "/auth/signin" });
-    }
-  }, [isAuthenticated, isLoading, navigate]);
-
-  if (isLoading) return null;
-
-  if (!isAuthenticated) return null;
-
   return (
     <DashboardLayout>
       <div>Hello "/dashboard/settings"!</div>
