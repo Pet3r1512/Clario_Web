87fdaff2fee9dc0c8ef8c2e9a4b27c3810c77148
Peter Pham <pttp15122002@gmail.com>
2025-12-16 20:02:01 +1100

    test: shorten correctly user's name

---
+++ src/helpers/shortenUserName.test.tsx
@@ -0,0 +1,25 @@
+import { describe, expect, it } from "vitest";
+import ShortenUserName from "./shortenUserName";
+
+describe("Shorten User Name helper's test: ", () => {
+  it("should return P when User Name is Peter", () => {
+    const UserName = "Peter";
+    const result = ShortenUserName(UserName);
+
+    expect(result).toBe("P");
+  });
+
+  it("should return JD when User Name is John Dean", () => {
+    const UserName = "John Dean";
+    const result = ShortenUserName(UserName);
+
+    expect(result).toBe("JD");
+  });
+
+  it("should return CP when User Name is Carrick Pattrick Alan Walker", () => {
+    const UserName = "Carrick Pattrick Alan Walker";
+    const result = ShortenUserName(UserName);
+
+    expect(result).toBe("CP");
+  });
+});
