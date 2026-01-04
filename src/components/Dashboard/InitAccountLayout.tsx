9e9058e478ea5eb1f237f8d45dd403f0fe99e4c0
Peter Pham <pttp15122002@gmail.com>
2026-01-04 14:40:56 +1100

    ops: create default balance for new user

--- src/components/Dashboard/InitAccountLayout.tsx
+++ src/components/Dashboard/InitAccountLayout.tsx
@@ -1,6 +1,44 @@
+/* eslint-disable react-hooks/exhaustive-deps */
+import createBalance, { Currency } from "@/api/users/createBalance";
+import useFetchUser from "@/hooks/useFetchUser";
+import { useMutation } from "@tanstack/react-query";
+import { useRouter } from "@tanstack/react-router";
 import { LoaderCircle } from "lucide-react";
+import { useEffect } from "react";
+import { toast } from "sonner";
 
-export default function InitAccount() {
+export default function InitAccount({
+  setCreatingAccount,
+}: {
+  setCreatingAccount: React.Dispatch<React.SetStateAction<boolean>>;
+}) {
+  const router = useRouter();
+  const userId = useFetchUser();
+
+  const mutation = useMutation({
+    mutationKey: ["balance"],
+    mutationFn: createBalance,
+    onError: (error) => {
+      setCreatingAccount(false);
+      return toast.error(error.message);
+    },
+    onSuccess: () => {
+      setCreatingAccount(false);
+      return router.navigate({ to: "/dashboard" });
+    },
+  });
+
+  useEffect(() => {
+    // prevent creating double records in balance table
+    if (!userId) return;
+    if (mutation.isPending) return;
+
+    mutation.mutate({
+      userId: userId,
+      currency: Currency.AUD,
+    });
+  }, [userId]);
+
   return (
     <center className="space-y-4 h-full">
       <p className="text-2xl lg:text-3xl font-bold text-primary">
