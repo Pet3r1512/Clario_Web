9ef3a1e7739a1472d5f96a4f65e7f1feb5a2d54a
Peter Pham <pttp15122002@gmail.com>
2026-01-11 00:11:58 +1100

    feat: transaction page's header

---
+++ src/components/Dashboard/Transactions/TransactionsHeader.tsx
@@ -0,0 +1,10 @@
+export default function TransactionsHeader() {
+  return (
+    <div className="space-y-1 md:space-y-2 lg:space-y-3">
+      <h1 className="text-xl md:text-2xl lg:text-3xl font-bold text-primary">
+        Transactions
+      </h1>
+      <p>Track and manage all your financial activities</p>
+    </div>
+  );
+}
