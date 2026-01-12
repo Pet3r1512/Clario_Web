99451e79eafdf5e97058def2133c420555d19bdf
Peter Pham <pttp15122002@gmail.com>
2026-01-12 19:19:39 +1100

    ops: fetch user's transactions

---
+++ src/components/Dashboard/Transactions/TransactionsTable/index.tsx
@@ -0,0 +1,21 @@
+import { useQuery } from "@tanstack/react-query";
+import ListByDate from "./ListByDate";
+import getTransactions from "@/api/users/transactions/getTransactions";
+
+interface TransactionsTableProps {
+  userId: string | undefined;
+}
+
+export default function TransactionsTable({ userId }: TransactionsTableProps) {
+  const { data, isLoading, isError, error } = useQuery({
+    queryKey: ["transactions", userId],
+    queryFn: () => getTransactions({ userId }),
+    enabled: !!userId,
+  });
+
+  if (!userId) return null; // or loading skeleton
+  if (isLoading) return <div>Loading...</div>;
+  if (isError) return <div>{(error as Error).message}</div>;
+
+  return <ListByDate transactions={data?.transactions.transactions} />;
+}
