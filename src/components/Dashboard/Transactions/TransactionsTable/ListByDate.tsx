abecc20ca2a4fdc5ed76f568883fe23442fb904a
Peter Pham <pttp15122002@gmail.com>
2026-01-13 22:59:52 +1100

    feat: display transactions by date

--- src/components/Dashboard/Transactions/TransactionsTable/ListByDate.tsx
+++ src/components/Dashboard/Transactions/TransactionsTable/ListByDate.tsx
@@ -30,16 +30,22 @@ export default function ListByDate({
   if (Object.keys(groupedByDate).length === 0) {
     return <div className="text-gray-500">No transactions</div>;
   }
+
   return (
-    <div className="space-y-5">
-      <div className="p-1.5 rounded-lg bg-gray-200 text-black font-semibold lg:hover:pl-5 lg:hover:bg-primary/50 transition-all duration-150 ease-linear">
-        Friday, 09 Jan 2026
-      </div>
-      <div className="space-y-5">
-        {transactions.map((transaction, index) => {
-          return <TransactionSummary key={index} transaction={transaction} />;
-        })}
-      </div>
-    </div>
+    <>
+      {Object.entries(groupedByDate)
+        .sort(([a], [b]) => b.localeCompare(a))
+        .map(([date, txs]) => (
+          <div key={date} className="space-y-5">
+            <div className="p-1.5 rounded-lg bg-gray-200 font-semibold">
+              {date}
+            </div>
+
+            {txs.map((tx) => (
+              <TransactionSummary key={tx.id} transaction={tx} />
+            ))}
+          </div>
+        ))}
+    </>
   );
 }
