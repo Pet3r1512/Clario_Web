3e68c7dcb58f5c9976bc733c4956867011e1c4fc
Peter Pham <pttp15122002@gmail.com>
2026-04-04 19:47:25 +1100

    ops: add cloudflare plugin

--- vite.config.ts
+++ vite.config.ts
@@ -2,14 +2,15 @@ import { defineConfig } from "vite";
 import path from "path";
 import react from "@vitejs/plugin-react";
 import { tanstackRouter } from "@tanstack/router-plugin/vite";
+import { cloudflare } from "@cloudflare/vite-plugin";
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [
     tanstackRouter({
       routesDirectory: "src/routes",
     }),
     react(),
+    cloudflare(),
   ],
   resolve: {
     alias: {
