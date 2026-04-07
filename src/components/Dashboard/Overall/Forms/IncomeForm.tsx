e8b409265ed95b5c00392ccf3e65509fd24cd483
Peter Pham <pttp15122002@gmail.com>
2026-04-07 21:51:50 +1000

    feat: dialog to create a new income

---
+++ src/components/Dashboard/Overall/Forms/IncomeForm.tsx
@@ -0,0 +1,65 @@
+import { Button } from "@/components/ui/button";
+import { DatePicker } from "@/components/ui/date-picker";
+import {
+  Dialog,
+  DialogClose,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
+import { Field, FieldGroup } from "@/components/ui/field";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+
+export function IncomeForm() {
+  return (
+    <Dialog>
+      <form>
+        <DialogTrigger asChild>
+          <Button className="bg-primary hover:bg-primary/90 text-white rounded-2xl text-lg h-10.5">
+            + Add Income
+          </Button>
+        </DialogTrigger>
+        <DialogContent className="sm:max-w-sm bg-white">
+          <DialogHeader>
+            <DialogTitle>Add Income</DialogTitle>
+          </DialogHeader>
+          <DialogDescription></DialogDescription>
+          <FieldGroup>
+            <Field>
+              <Label htmlFor="amount">Amount</Label>
+              <Input
+                id="amount"
+                name="amount"
+                type="text"
+                min={1}
+                inlist={"decimal"}
+              />
+            </Field>
+            <Field>
+              <Label htmlFor="source">Source</Label>
+              <Input
+                id="source"
+                name="source"
+                placeholder="e.g. Salary, Family Support, etc."
+              />
+            </Field>
+            <Field>
+              <Label htmlFor="date">Date</Label>
+              <DatePicker />
+            </Field>
+          </FieldGroup>
+          <DialogFooter>
+            <DialogClose asChild>
+              <Button variant="outline">Cancel</Button>
+            </DialogClose>
+            <Button type="submit">Save changes</Button>
+          </DialogFooter>
+        </DialogContent>
+      </form>
+    </Dialog>
+  );
+}
