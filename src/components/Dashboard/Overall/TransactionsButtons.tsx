819f226f1aee694970baf89504a82f4fbdf907c9
Peter Pham <pttp15122002@gmail.com>
2026-04-07 21:52:04 +1000

    update: use dialog's trigger instead

--- src/components/Dashboard/Overall/TransactionsButtons.tsx
+++ src/components/Dashboard/Overall/TransactionsButtons.tsx
@@ -1,11 +1,10 @@
 import { Button } from "@/components/ui/button";
+import { IncomeForm } from "./Forms/IncomeForm";
 
 export default function TransactionButtons() {
   return (
     <div className="flex items-center gap-x-5 lg:justify-end">
-      <Button className="bg-primary hover:bg-primary/90 text-white rounded-2xl text-lg h-10.5">
-        + Add Income
-      </Button>
+      <IncomeForm />
       <Button className="bg-white text-black border-[0.5px] border-black hover:bg-white/90 rounded-2xl text-lg h-10.5">
         + Add Expense
       </Button>
