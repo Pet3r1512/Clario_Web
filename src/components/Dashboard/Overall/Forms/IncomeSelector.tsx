2a82ea6b1bff30079855e678ac6c86d177bdd224
Peter Pham <89619124+Pet3r1512@users.noreply.github.com>
2026-04-12 20:20:40 +1000

    #FEAT-024: add income form (#68)
    * add: components from shadcn
    
    * dist: install dependencies for date picker
    
    * feat: dialog to create a new income
    
    * update: use dialog's trigger instead
    
    * ops: upgrade to react 19
    
    * fix: forward ref from radix
    
    * fix: errors
    
    * fix: warning for aria-hidden
    
    * imp: move form inside dialog content instead
    
    * fix: set modal is false
    
    * chore: hide pricing from nav bar
    
    * fix: warning about aria-hidden
    
    * dist: reinstall
    
    * add: select component from shadcn
    
    * chore
    
    * chore: select component from shadcn
    
    * add: component to select income source
    
    * feat: use income source selector instead
    
    * chore: remove unused
    
    * imp: set default value is today
    
    * chore: import directly from react instead
    
    * chore: remove unused
    
    * style
    
    * ops: fetch to create new income
    
    * style: highlighted today with gray background color
    
    * ops: register value for createdAt in the form
    
    * ops: register value for categoryId in the form
    
    * fix: use setValue to non-native component
    
    * fix: use ISO string instead
    
    * fix: duplicated keys
    
    * fix: use number instead of string
    
    * fix: use form context instead
    
    * feat: add income form
    
    * fix: missing jsx

---
+++ src/components/Dashboard/Overall/Forms/IncomeSelector.tsx
@@ -0,0 +1,77 @@
+import {
+  Select,
+  SelectContent,
+  SelectGroup,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
+import { useFormContext } from "react-hook-form";
+import { IncomeTransaction } from "./IncomeForm";
+
+type SourceIncome = {
+  name: string;
+  id: number;
+};
+
+export default function IncomeSelect() {
+  const { setValue } = useFormContext<IncomeTransaction>();
+
+  const incomesources: SourceIncome[] = [
+    {
+      name: "Salary",
+      id: 1,
+    },
+    {
+      name: "Bonus",
+      id: 2,
+    },
+    {
+      name: "Freelance",
+      id: 3,
+    },
+    {
+      name: "Investment",
+      id: 4,
+    },
+    {
+      name: "Retail Income",
+      id: 5,
+    },
+    {
+      name: "Gift",
+      id: 6,
+    },
+    {
+      name: "Other",
+      id: 7,
+    },
+  ];
+
+  return (
+    <Select
+      onValueChange={(value) => {
+        setValue("categoryId", +value);
+      }}
+    >
+      <SelectTrigger id="source" name="source" className="w-full">
+        <SelectValue />
+      </SelectTrigger>
+      <SelectContent className="bg-white w-full">
+        <SelectGroup>
+          {incomesources.map((source: SourceIncome) => {
+            return (
+              <SelectItem
+                key={source.id}
+                value={source.id.toString()}
+                className="lg:hover:bg-gray-200"
+              >
+                {source.name}
+              </SelectItem>
+            );
+          })}
+        </SelectGroup>
+      </SelectContent>
+    </Select>
+  );
+}
