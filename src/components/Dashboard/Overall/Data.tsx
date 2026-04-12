f0d5924bd17226cf1ae10fc03a5578d79effa9bb
Peter Pham <pttp15122002@gmail.com>
2026-04-13 07:39:19 +1000

    imp: display skeleton while waiting for balance

--- src/components/Dashboard/Overall/Data.tsx
+++ src/components/Dashboard/Overall/Data.tsx
@@ -1,16 +1,8 @@
 import { Card } from "@/components/ui/card";
-import { ReactNode } from "react";
+import { OverallDataType } from ".";
+import { Skeleton } from "@/components/ui/skeleton";
 
-export default function Data({
-  data,
-}: {
-  data: {
-    name: string;
-    subtitle: string;
-    icon: ReactNode;
-    amount: number;
-  };
-}) {
+export default function Data({ data }: { data: OverallDataType }) {
   return (
     <Card className="w-full py-3.5 px-5 gap-2.5 shadow-lg lg:hover:shadow-2xl transition-all duration-150 ease-linear">
       <div className="flex items-center justify-between">
@@ -18,7 +10,11 @@ export default function Data({
         {data.icon}
       </div>
       <p className="lg:text-xl font-semibold">
-        ${Number(data.amount ?? 0).toFixed(2)}
+        {data.isLoading ? (
+          <Skeleton className="w-1/2 h-7" />
+        ) : (
+          `${Number(data.amount ?? 0).toFixed(2)}`
+        )}
       </p>
       <p className="text-gray-400 lg:text-sm">{data.subtitle}</p>
     </Card>
