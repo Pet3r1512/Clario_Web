1df0d0f4b92a266ccabd7c0eac062e28b4f61030
Peter Pham <pttp15122002@gmail.com>
2026-04-26 16:17:33 +1000

    imp: add new page prop

--- src/api/users/transactions/getTransactions.ts
+++ src/api/users/transactions/getTransactions.ts
@@ -2,12 +2,12 @@ import { SERVER_URL } from "@/constant/auth";
 
 export default async function getTransactions(input?: {
   userId?: string;
-  month?: string;
+  page: number;
 }) {
   const encodedInput = encodeURIComponent(JSON.stringify(input ?? {}));
 
   const response = await fetch(
-    `${SERVER_URL}/api/trpc/transactions.getTransactions?input=${encodedInput}`,
+    `${SERVER_URL}/api/trpc/transactions.getTransactions?input=${encodedInput}&page=${input?.page}`,
     {
       method: "GET",
       credentials: "include",
