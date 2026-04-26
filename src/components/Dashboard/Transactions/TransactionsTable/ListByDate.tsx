6e2e467afaa5f0b0f69a0fb1fc5f5ab41ef4686c
Peter Pham <pttp15122002@gmail.com>
2026-04-26 16:36:41 +1000

    fix: sort the entries before displaying it

--- src/components/Dashboard/Transactions/TransactionsTable/ListByDate.tsx
+++ src/components/Dashboard/Transactions/TransactionsTable/ListByDate.tsx
@@ -17,8 +17,7 @@ export default function ListByDate({
   lastElementRef,
 }: {
   transactions?: TransactionInfo[];
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  lastElementRef: any;
+  lastElementRef: (node: HTMLDivElement | null) => void;
 }) {
   const groupedByDate = groupTransactions(transactions);
 
@@ -26,28 +25,31 @@ export default function ListByDate({
     return <div className="text-gray-500">No transactions</div>;
   }
 
-  const allTxs = Object.entries(groupedByDate)
-    .sort(([a], [b]) => b.localeCompare(a))
-    // eslint-disable-next-line @typescript-eslint/no-unused-vars
-    .flatMap(([_, txs]) => txs);
+  const sortedEntries = Object.entries(groupedByDate).sort(([a], [b]) =>
+    b.localeCompare(a),
+  );
 
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  const allTxs = sortedEntries.flatMap(([_, txs]) => txs);
   const lastTxId = allTxs[allTxs.length - 1]?.id;
 
   return (
     <section className="space-y-10 max-h-[82.5%] overflow-y-auto">
-      {Object.entries(groupedByDate)
-        .sort(([a], [b]) => b.localeCompare(a))
-        .map(([date, txs]) => (
-          <div key={date}>
-            {txs.map((tx) => (
-              <TransactionSummary
-                key={tx.id}
-                transaction={tx}
-                lastElementRef={tx.id === lastTxId ? lastElementRef : undefined}
-              />
-            ))}
+      {sortedEntries.map(([date, txs]) => (
+        <div key={date} className="space-y-5">
+          <div className="p-1.5 rounded-lg bg-gray-200 font-semibold">
+            {date}
           </div>
-        ))}
+
+          {txs.map((tx, index) => (
+            <TransactionSummary
+              key={tx.id ?? `${tx.date}-${tx.amount}-${index}`}
+              transaction={tx}
+              lastElementRef={tx.id === lastTxId ? lastElementRef : undefined}
+            />
+          ))}
+        </div>
+      ))}
     </section>
   );
 }
