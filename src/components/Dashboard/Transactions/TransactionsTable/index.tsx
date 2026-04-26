c12c37535bc2e4206dc655ebd93b39a845d24c96
Peter Pham <pttp15122002@gmail.com>
2026-04-26 18:18:43 +1000

    chore: remove unused

--- src/components/Dashboard/Transactions/TransactionsTable/index.tsx
+++ src/components/Dashboard/Transactions/TransactionsTable/index.tsx
@@ -1,4 +1,3 @@
-/* eslint-disable @typescript-eslint/no-explicit-any */
 import { useInfiniteQuery } from "@tanstack/react-query";
 import ListByDate, { TransactionInfo } from "./ListByDate";
 import getTransactions from "@/api/users/transactions/getTransactions";
