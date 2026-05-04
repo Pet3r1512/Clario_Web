0852ee536c062c415d9335d594c64da9893e961a
Peter Pham <89619124+Pet3r1512@users.noreply.github.com>
2026-05-04 21:36:08 +1000

    #FIX-014: transaction table on mobile (#76)
    * fix: coming soon btn is on top of sidebar
    
    * fix: use section attribute instead
    
    * chore: remove unused
    
    * chore: use tailwind config instead of fixed number
    
    * style: fix unstable height of table on mobiles

--- src/components/Auth/SignInViaGoogleBtn.tsx
+++ src/components/Auth/SignInViaGoogleBtn.tsx
@@ -3,7 +3,7 @@ import { Button } from "../ui/button";
 
 export default function SignInViaGoogleBtn() {
   return (
-    <div className="flex flex-col gap-4 relative">
+    <div className="flex flex-col gap-4 relative z-1">
       <p className="absolute -top-3 z-100 -right-4.5 text-white rounded-2xl font-semibold text-xs bg-blue-400 px-1.5 py-0.5">
         Coming Soon
       </p>
