e302700d08f89b26de50e864b25cf307765e543c
Peter Pham <89619124+Pet3r1512@users.noreply.github.com>
2026-05-07 13:32:11 +1000

    #IMP-019: update profile setting section (#80)
    * add: new button
    
    * chore: remove unused
    
    * feat: use logout function
    
    * style: red text for log out button

--- src/components/Dashboard/Sidebar/SidebarFooter.tsx
+++ src/components/Dashboard/Sidebar/SidebarFooter.tsx
@@ -1,10 +1,8 @@
 import { SidebarMenuButton } from "@/components/ui/sidebar";
-import { Settings, MessageCircleQuestion } from "lucide-react";
-import { User } from "./User";
+import { Settings, MessageCircleQuestion, LogOut } from "lucide-react";
 import { Link } from "@tanstack/react-router";
 import { cn } from "@/lib/utils";
-import ShortenUserName from "@/helpers/shortenUserName";
-import useFetchSession from "@/hooks/useFetchSession";
+import { useLogout } from "@/hooks/useLogOut";
 
 const items = [
   {
@@ -17,16 +15,25 @@ const items = [
     url: "/dashboard/helps",
     icon: <MessageCircleQuestion />,
   },
+  {
+    title: "Log Out",
+    url: "",
+    icon: <LogOut />,
+  },
 ];
 
 export default function SidebarFooter({ currUrl }: { currUrl: string }) {
-  const sessionQuery = useFetchSession();
+  // const sessionQuery = useFetchSession();
+  const { logout } = useLogout();
 
   return (
     <section className="px-5 pb-10">
       <div className="space-y-2.5">
         {items.map((item) => (
-          <div key={item.title}>
+          <div
+            key={item.title}
+            onClick={item.title === "Log Out" ? logout : () => {}}
+          >
             <SidebarMenuButton
               asChild
               className={cn(
@@ -34,6 +41,9 @@ export default function SidebarFooter({ currUrl }: { currUrl: string }) {
                 currUrl === item.url
                   ? "bg-primary text-white lg:hover:bg-primary lg:hover:text-white"
                   : "lg:hover:bg-gray-100",
+                item.title === "Log Out"
+                  ? "text-red-500 hover:text-red-600 transition-all duration-150 ease-linear"
+                  : "",
               )}
             >
               <Link from="/" to={item.url} className="text-lg font-semibold">
@@ -44,14 +54,14 @@ export default function SidebarFooter({ currUrl }: { currUrl: string }) {
           </div>
         ))}
       </div>
-      <User
+      {/* <User
         user={{
           name: sessionQuery.data?.data?.user.name ?? "",
           email: sessionQuery.data?.data?.user.email ?? "",
           avatar: sessionQuery.data?.data?.user.image ?? "",
           shortenName: ShortenUserName(sessionQuery.data?.data?.user.name),
         }}
-      />
+      /> */}
     </section>
   );
 }
