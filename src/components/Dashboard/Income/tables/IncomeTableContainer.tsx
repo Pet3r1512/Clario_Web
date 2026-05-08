a2601d47f3e74cbffcb8325f91dd50b3ffaae416
Peter Pham <pttp15122002@gmail.com>
2026-05-08 12:41:53 +1000

    feat: use custom hook to get all income transactions

--- src/components/Dashboard/Income/tables/IncomeTableContainer.tsx
+++ src/components/Dashboard/Income/tables/IncomeTableContainer.tsx
@@ -1,6 +1,10 @@
+import useFetchTransactions from "@/hooks/useFetchTransactions";
 import IncomeTable from "./IncomeTable";
 
 export default function IncomeTableContainer() {
+  const { data } = useFetchTransactions({ option: "onlyIncome" });
+
+  console.log(data);
   return (
     <section className="bg-white rounded-2xl shadow-2xl p-5 h-[70dvh] overflow-auto">
       <p className="text-lg lg:text-xl font-bold">Income Sources</p>
